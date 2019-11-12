0d8e8585bd9b3e7bcbcff2b108f2d14e3917b21e
allan0liu <2391154853@qq.com>
2019-11-12 21:25:07 +0800

    学完struts

--- src/main/java/com/allan/struts2/StrutsDemo1.java
+++ src/main/java/com/allan/struts2/StrutsDemo1.java
@@ -1,8 +1,23 @@
 package com.allan.struts2;
 
+import com.allan.entity.User;
+import ognl.Ognl;
+import ognl.OgnlContext;
+import ognl.OgnlException;
+
 public class StrutsDemo1 {
-    public String execute(){
-        System.out.println("exe....");
+    public String execute() throws OgnlException {
+        OgnlContext ognlContext = new OgnlContext();
+        ognlContext.put("name", "allan");
+        String name = (String)Ognl.getValue("#name", ognlContext, ognlContext.getRoot());
+        System.out.println(name);
+
+        //获取root中的数据
+        User user = new User();
+        user.setName("fier");
+        ognlContext.setRoot(user);
+        String name2 = (String)Ognl.getValue("name", ognlContext, ognlContext.getRoot());
+        System.out.println(name2);
         return "success";
     }
 }
