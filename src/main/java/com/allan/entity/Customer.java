27c2a49f7e1e0d33b3f46e87ed7505494dc634c1
fierliu <fierliu@126.com>
2019-11-13 17:29:47 +0800

    hibernate day1 page16

---
+++ src/main/java/com/allan/entity/Customer.java
@@ -0,0 +1,51 @@
+package com.allan.entity;
+
+public class Customer {
+
+    private int id;
+    private String name;
+    private String address;
+    private String sex;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", sex='" + sex + '\'' +
+                '}';
+    }
+}
